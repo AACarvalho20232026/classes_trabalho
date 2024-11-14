9b6dc6bf2c8ea5f81f430f555fe520519e0e2bb7
AACarvalho20232026 <145613828+AACarvalho20232026@users.noreply.github.com>
2024-11-14 20:32:18 -0100

    Update Moto.h

--- Moto.h
+++ Moto.h
@@ -8,7 +8,7 @@ private:
     string tipoGuidao;
 
 public:
-    Moto(const string& nomeMoto, const string& guidao);
+    Moto(const string nomeMoto, const string guidao);
     void mostrarDetalhes() ;
     void salvarDetalhes(ofstream& arquivo) ;
 };

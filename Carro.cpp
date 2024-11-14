0ecea211a8df324f37634ce059bf7881267fc0c6
AACarvalho20232026 <145613828+AACarvalho20232026@users.noreply.github.com>
2024-11-14 20:33:30 -0100

    Update Carro.cpp

--- Carro.cpp
+++ Carro.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-Carro::Carro(const string& nomeCarro, int portas) : Veiculo(nomeCarro), numeroPortas(portas) {}
+Carro::Carro(const string nomeCarro, int portas) : Veiculo(nomeCarro), numeroPortas(portas) {}
 
 void Carro::mostrarDetalhes() {
     cout << "Carro: " << nome << ", Número de portas: " << numeroPortas << endl;
